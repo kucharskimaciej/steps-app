bfe554314a2630605326fe038b72b337a14d6275
Maciej Kucharski <maciek.zir+github@gmail.com>
2019-08-13 17:37:23 +0200

    [master] added tailwind, no theme for now

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,5 +1,27 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const atimport = require("postcss-import");
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
-}
+    plugins: [
+        atimport(),
+        tailwindcss("./tailwind.config.js"),
+        autoprefixer({
+            add: true,
+            grid: true
+        }),
+        process.env.NODE_ENV === "production"? purgecss({
+            content: ['./src/**/*.vue'],
+            extractors: [
+                {
+                    extractor: class TailwindExtractor {
+                        static extract(content) {
+                            return content.match(/[A-z0-9-:\/]+/g) || [];
+                        }
+                    },
+                    extensions: ['vue']
+                }
+            ]
+        }) : null
+    ]
+};
