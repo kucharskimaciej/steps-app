a0b2e3011eab2c83d8e9634bd296eaf0556703c6
Maciej Kucharski <maciek.zir+github@gmail.com>
2020-08-01 22:46:50 +0200

    [master][fix]

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -15,6 +15,34 @@ module.exports = {
     process.env.NODE_ENV === "production"
       ? purgecss({
           content: ["./src/**/*.vue"],
+          whitelist: [
+            /* buttons colors classes */
+            ..._.flatten(
+              ["blue", "green", "red", "yellow"].map(color => {
+                return [
+                  `bg-${color}-base`,
+                  `border-${color}-base`,
+                  `hover:bg-${color}-light`,
+                  `hover:border-${color}-light`,
+                  `text-white`,
+                  `active:shadow-inner`,
+                  `focus:shadow`,
+                  `bg-transparent`,
+                  `text-${color}-base`,
+                  `hover:text-${color}-light`,
+                  `border-transparent`,
+                  `hover:bg-gray-100`
+                ];
+              })
+            ),
+            /* buttons spacing classes */
+            ..."px-6 py-1 border text-sm".split(" "),
+            ..."px-3 py-1 border text-sm".split(" "),
+            ..."px-4 py-1 border text-2xs".split(" "),
+            ..."px-2 py-1 border text-2xs".split(" "),
+            ..."px-10 py-1 border-2 text-xl".split(" "),
+            ..."px-4 py-1 border-2 text-xl".split(" ")
+          ],
           extractors: [
             {
               extractor: class TailwindExtractor {
@@ -22,34 +50,6 @@ module.exports = {
                   return content.match(/[A-z0-9-:\/]+/g) || [];
                 }
               },
-              whitelist: [
-                /* buttons colors classes */
-                ..._.flatten(
-                  ["blue", "green", "red", "yellow"].map(color => {
-                    return [
-                      `bg-${color}-base`,
-                      `border-${color}-base`,
-                      `hover:bg-${color}-light`,
-                      `hover:border-${color}-light`,
-                      `text-white`,
-                      `active:shadow-inner`,
-                      `focus:shadow`,
-                      `bg-transparent`,
-                      `text-${color}-base`,
-                      `hover:text-${color}-light`,
-                      `border-transparent`,
-                      `hover:bg-gray-100`
-                    ];
-                  })
-                ),
-                /* buttons spacing classes */
-                ..."px-6 py-1 border text-sm".split(" "),
-                ..."px-3 py-1 border text-sm".split(" "),
-                ..."px-4 py-1 border text-2xs".split(" "),
-                ..."px-2 py-1 border text-2xs".split(" "),
-                ..."px-10 py-1 border-2 text-xl".split(" "),
-                ..."px-4 py-1 border-2 text-xl".split(" ")
-              ],
               extensions: ["vue"]
             }
           ]
