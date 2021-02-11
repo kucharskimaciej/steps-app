284838512a4347e987927866aa068c806605e707
Maciej Kucharski <maciek.zir+github@gmail.com>
2021-02-11 02:42:23 +0100

    [master] don't minimize classnames, as modal components depend on those

--- web/vue.config.js
+++ web/vue.config.js
@@ -3,5 +3,12 @@ module.exports = {
     devServer: {
       headers: { "Access-Control-Allow-Origin": "*" }
     }
+  },
+  chainWebpack: config => {
+    config.optimization.minimizer("terser").tap(args => {
+      const { terserOptions } = args[0];
+      terserOptions.keep_classnames = true;
+      return args;
+    });
   }
 };
