c73b403a9affe32b1f16f08fc2a223e5315f609b
Maciej Kucharski <maciek.zir+github@gmail.com>
2021-03-29 02:15:00 +0200

    [step-creation-tweaks] migration to rename dance field

---
+++ scripts/migrate/lib/migrations/003-rename-dance-to-feeling.ts
@@ -0,0 +1,18 @@
+import { RunFn } from "../types";
+
+const run: RunFn = async db => {
+  const allSteps = await db.collection("steps").get();
+  allSteps.forEach(doc => {
+    const step = doc.data() as { dance: string };
+
+    doc.ref.set(
+      {
+        feeling: step.dance,
+        dance: null
+      },
+      { merge: true }
+    );
+  });
+};
+
+export default run;
