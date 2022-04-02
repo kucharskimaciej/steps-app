ad0ee2626f74a3e07348387fba66e0661b90e0df
Maciej Kucharski <maciek.zir+github@gmail.com>
2022-04-02 15:21:43 +0200

    [master] disable session-related views

--- src/views/HistoricalSession/HistoricalSessionView.vue
+++ src/views/HistoricalSession/HistoricalSessionView.vue
@@ -1,32 +1,40 @@
-<script lang="ts">
-import { Component, Prop } from "vue-property-decorator";
-import { VueWithStore } from "@/lib/vueWithStore";
-import Container from "@/components/Layout/Container.vue";
-import AllStepsProvider from "../../components/Providers/AllStepsProvider";
-import Feed from "@/features/Feed/Feed.vue";
-import { Step } from "../../../common/types/Step";
-import { stepsByPracticeDate } from "@/store";
-
-@Component({
-  components: {
-    AllStepsProvider,
-    Container,
-    Feed
-  }
-})
-export default class HistoricalSessionView extends VueWithStore {
-  @Prop({ type: [String, Number] }) private sessionDate!: string | number;
-
-  get stepsInSession(): Step[] {
-    return stepsByPracticeDate(this.$store)[Number(this.sessionDate)];
-  }
-}
+<script>
+// @todo
+export default {};
 </script>
 
 <template>
-  <AllStepsProvider>
-    <Container class="pt-5 pb-10">
-      <Feed :steps="stepsInSession" />
-    </Container>
-  </AllStepsProvider>
+  <span />
 </template>
+<!--<script lang="ts">-->
+<!--import { Component, Prop } from "vue-property-decorator";-->
+<!--import { VueWithStore } from "@/lib/vueWithStore";-->
+<!--import Container from "@/components/Layout/Container.vue";-->
+<!--import AllStepsProvider from "../../components/Providers/AllStepsProvider";-->
+<!--import Feed from "@/features/Feed/Feed.vue";-->
+<!--import { Step } from "../../../common/types/Step";-->
+<!--import { stepsByPracticeDate } from "@/store";-->
+
+<!--@Component({-->
+<!--  components: {-->
+<!--    AllStepsProvider,-->
+<!--    Container,-->
+<!--    Feed-->
+<!--  }-->
+<!--})-->
+<!--export default class HistoricalSessionView extends VueWithStore {-->
+<!--  @Prop({ type: [String, Number] }) private sessionDate!: string | number;-->
+
+<!--  get stepsInSession(): Step[] {-->
+<!--    return stepsByPracticeDate(this.$store)[Number(this.sessionDate)];-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <AllStepsProvider>-->
+<!--    <Container class="pt-5 pb-10">-->
+<!--      <Feed :steps="stepsInSession" />-->
+<!--    </Container>-->
+<!--  </AllStepsProvider>-->
+<!--</template>-->
