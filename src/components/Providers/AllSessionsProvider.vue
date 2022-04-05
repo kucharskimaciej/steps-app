693db342572fb7772ae9c1cfb68243b4a61876ae
Maciej Kucharski <maciek.zir+github@gmail.com>
2022-04-05 17:10:34 +0200

    [master] comment out data providers for sessions

--- src/components/Providers/AllSessionsProvider.vue
+++ src/components/Providers/AllSessionsProvider.vue
@@ -1,38 +1,43 @@
-<script lang="ts">
-import { Component, Prop } from "vue-property-decorator";
-import { VueWithStore } from "@/lib/vueWithStore";
-import { dispatchFetchAllSessions } from "@/store";
+<!--<script lang="ts">-->
+<!--import { Component, Prop } from "vue-property-decorator";-->
+<!--import { VueWithStore } from "@/lib/vueWithStore";-->
+<!--import { dispatchFetchAllSessions } from "@/store";-->
 
-@Component
-export default class AllSessionsProvider extends VueWithStore {
-  @Prop() private alwaysFetch!: boolean;
+<!--@Component-->
+<!--export default class AllSessionsProvider extends VueWithStore {-->
+<!--  @Prop() private alwaysFetch!: boolean;-->
 
-  get loading() {
-    return this.$store.state.practiceSessions.status === "pending";
-  }
+<!--  get loading() {-->
+<!--    return this.$store.state.practiceSessions.status === "pending";-->
+<!--  }-->
 
-  async load() {
-    if (
-      !this.alwaysFetch &&
-      this.$store.state.practiceSessions.status !== "clean"
-    ) {
-      return;
-    }
+<!--  async load() {-->
+<!--    if (-->
+<!--      !this.alwaysFetch &&-->
+<!--      this.$store.state.practiceSessions.status !== "clean"-->
+<!--    ) {-->
+<!--      return;-->
+<!--    }-->
 
-    await dispatchFetchAllSessions(this.$store);
-  }
+<!--    await dispatchFetchAllSessions(this.$store);-->
+<!--  }-->
 
-  async created() {
-    await this.load();
-  }
-}
+<!--  async created() {-->
+<!--    await this.load();-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <slot v-if="loading" name="loading">-->
+<!--      <h1>Loading sessions</h1>-->
+<!--    </slot>-->
+<!--    <slot v-else />-->
+<!--  </div>-->
+<!--</template>-->
+
+<script>
+export default {};
 </script>
-
-<template>
-  <div>
-    <slot v-if="loading" name="loading">
-      <h1>Loading sessions</h1>
-    </slot>
-    <slot v-else />
-  </div>
-</template>
+<template><span></span></template>
--- src/components/Providers/SessionProvider.vue
+++ src/components/Providers/SessionProvider.vue
@@ -1,46 +1,50 @@
-<script lang="ts">
-import { Component, Prop, Watch } from "vue-property-decorator";
-import { VueWithStore } from "@/lib/vueWithStore";
-import { dispatchFetchSession } from "@/store/modules/selectedSession";
+<!--<script lang="ts">-->
+<!--import { Component, Prop, Watch } from "vue-property-decorator";-->
+<!--import { VueWithStore } from "@/lib/vueWithStore";-->
+<!--import { dispatchFetchSession } from "@/store/modules/selectedSession";-->
 
-@Component
-export default class SessionProvider extends VueWithStore {
-  @Prop({ required: true }) private id!: string;
+<!--@Component-->
+<!--export default class SessionProvider extends VueWithStore {-->
+<!--  @Prop({ required: true }) private id!: string;-->
 
-  @Watch("id")
-  onStepIdChanged(newValue: string, oldValue: string) {
-    if (newValue && newValue !== oldValue) {
-      this.load(newValue);
-    }
-  }
+<!--  @Watch("id")-->
+<!--  onStepIdChanged(newValue: string, oldValue: string) {-->
+<!--    if (newValue && newValue !== oldValue) {-->
+<!--      this.load(newValue);-->
+<!--    }-->
+<!--  }-->
 
-  get loading() {
-    return this.$store.state.selectedSession.status === "pending";
-  }
+<!--  get loading() {-->
+<!--    return this.$store.state.selectedSession.status === "pending";-->
+<!--  }-->
 
-  get session() {
-    return this.$store.state.selectedSession.session;
-  }
+<!--  get session() {-->
+<!--    return this.$store.state.selectedSession.session;-->
+<!--  }-->
 
-  async load(id: string) {
-    if (this.loading) {
-      return;
-    }
+<!--  async load(id: string) {-->
+<!--    if (this.loading) {-->
+<!--      return;-->
+<!--    }-->
 
-    await dispatchFetchSession(this.$store, id);
-  }
+<!--    await dispatchFetchSession(this.$store, id);-->
+<!--  }-->
 
-  async created() {
-    await this.load(this.id);
-  }
-}
+<!--  async created() {-->
+<!--    await this.load(this.id);-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <slot v-if="loading" name="loading">-->
+<!--      <h1>Loading step</h1>-->
+<!--    </slot>-->
+<!--    <slot v-else />-->
+<!--  </div>-->
+<!--</template>-->
+<script>
+export default {};
 </script>
-
-<template>
-  <div>
-    <slot v-if="loading" name="loading">
-      <h1>Loading step</h1>
-    </slot>
-    <slot v-else />
-  </div>
-</template>
+<template><span></span></template>
